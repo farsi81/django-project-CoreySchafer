b0c14e35a992401fa7af8867e6cc3e37a749f116
Mohammad Hossein <mhf8100@gmail.com>
2022-04-06 18:52:30 +0430

    until end of part 12

--- myProject/settings.py
+++ myProject/settings.py
@@ -125,3 +125,10 @@ LOGIN_REDIRECT_URL = 'blog-home'
 LOGIN_URL = 'login'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = os.environ.get('EMAIL_USER')
+EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_PASS')
