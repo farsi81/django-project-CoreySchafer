3eb01b5e30071abc6e9f17530798e55f008775b1
Mohammad Hossein <mhf8100@gmail.com>
2022-04-06 17:16:55 +0430

    until end of part 11

--- blog/urls.py
+++ blog/urls.py
@@ -3,11 +3,13 @@ from .views import (PostListView,
                     PostDetailView,
                     PostCreateView,
                     PostUpdateView,
-                    PostDeleteView)
+                    PostDeleteView,
+                    UserPostListView)
 from . import views
 
 urlpatterns = [
     path("", PostListView.as_view(), name="blog-home"),
+    path("user/<str:username>", UserPostListView.as_view(), name="user_posts"),
     path('post/<int:pk>/', PostDetailView.as_view(), name="post_detail"),
     path('post/new/', PostCreateView.as_view(), name="post_create"),
     path('post/<int:pk>/update/', PostUpdateView.as_view(), name="post_update"),
